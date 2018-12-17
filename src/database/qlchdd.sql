92964b65b4eff09f04876eb2ee59e98dd1a9ba7f
ThuThaoDo <37336604+ThuThaoDo@users.noreply.github.com>
2018-12-17 10:17:33 +0700

    Update qlchdd.sql

--- src/database/qlchdd.sql
+++ src/database/qlchdd.sql
@@ -10,6 +10,60 @@ DELIMITER $$
 --
 -- Procedures
 --
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DoanhThu_Nam` ()  NO SQL
+SELECT Year(hoadonban.ngay_Ban) AS Nam, SUM(hoadonban.tongtien_Ban) AS TongDoanhThu
+	FROM hoadonban
+	Group by Nam$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DoanhThu_Ngay` ()  NO SQL
+SELECT Day(hoadonban.ngay_Ban) AS Ngay, SUM(hoadonban.tongtien_Ban) AS TongDoanhThu
+	FROM hoadonban
+	Group by Ngay$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DoanhThu_Thang` ()  NO SQL
+SELECT Month(hoadonban.ngay_Ban) AS Thang, SUM(hoadonban.tongtien_Ban) AS TongDoanhThu
+	FROM hoadonban
+	Group by Thang$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `HDBan_NgBan` (IN `NGBAN` DATE)  NO SQL
+SELECT * FROM hoadonban WHERE hoadonban.ngay_Ban = NGBAN$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `HDMua_NgNhap` (IN `NGNHAP` DATE)  NO SQL
+SELECT * FROM hoadonmua WHERE hoadonmua.ngay_Nhap = NGNHAP$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `KhachHang` (IN `TenKH` VARCHAR(45))  NO SQL
+SELECT * FROM khachhang WHERE ten_KH LIKE CONCAT('%',TENKH,'%')$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `NhanVien` (IN `TENNV` VARCHAR(45))  NO SQL
+SELECT * FROM nhanvien WHERE ten_NV LIKE CONCAT('%',TENNV,'%')$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SanPham` (IN `TENSP` VARCHAR(45))  NO SQL
+SELECT * FROM sanpham WHERE ten_SP LIKE CONCAT('%',TENSP,'%')$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `TK_NhanVien_DoanhThu` ()  NO SQL
+SELECT nv.ma_NV, nv.ten_NV,Month(hdb.ngay_Ban) Thang, Year(hdb.ngay_Ban)Nam, SUM(hdb.tongtien_Ban) TongTienBan
+	FROM nhanvien nv
+    LEFT JOIN hoadonban hdb on hdb.ma_NV = nv.ma_NV
+  
+	GROUP BY nv.ma_NV, Thang, Nam
+    ORDER BY nv.ma_NV$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `TK_SLSP_BanTrongThang-Nam` (IN `th` INT(11), IN `n` INT(11))  NO SQL
+SELECT sp.ma_SP, sp.ten_SP, sum(db.sl) sl
+FROM sanpham sp LEFT JOIN (SELECT ct.ma_SP FROM cthd_ban ct, hoadonban hd WHERE ct.sohd_Ban = hd.sohd_Ban and Month(hd.ngay_Ban) = th and year(hd.ngay_Ban) = n) as db ON sp.ma_SP = db.ma_SP
+
+
+GROUP BY sp.ma_SP$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `TK_TOP5SP_BanChay_Nam` ()  NO SQL
+SELECT ct.ma_SP, sp.ten_SP, SUM(ct.sl) SLBanRa
+FROM cthd_ban ct, sanpham sp
+WHERE sp.ma_SP = ct.ma_SP
+
+GROUP BY sp.ma_SP
+ORDER BY SLBanRa DESC
+LIMIT 5$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `USP_Login` (IN `tenDangNhap` VARCHAR(45), IN `matKhau` VARCHAR(45))  NO SQL
 SELECT * FROM taikhoan WHERE taikhoan.ten_DangNhap = tenDangNhap AND taikhoan.matkhau_DangNhap = matKhau$$
 
@@ -32,6 +86,14 @@ CREATE TABLE `baohanh` (
   `ngaytra` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `baohanh`
+--
+
+INSERT INTO `baohanh` (`ma_BH`, `sohd_Ban`, `ma_SP`, `serial`, `yeucau_BH`, `ngaynhan`, `tinhtrang`, `ngaytra`) VALUES
+(1, 1, 1, 12345, '', '2018-12-14 00:00:00', 0, '2018-12-19 00:00:00'),
+(2, 2, 2, 3456, '', '2018-12-13 00:00:00', 0, '2018-12-17 00:00:00');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +108,15 @@ CREATE TABLE `cthd_ban` (
   `thanhtien` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `cthd_ban`
+--
+
+INSERT INTO `cthd_ban` (`sohd_Ban`, `ma_SP`, `sl`, `ma_KM`, `thanhtien`) VALUES
+(1, 1, 1, 1, NULL),
+(2, 2, 2, 1, 25000000),
+(2, 3, 1, 1, 16000000);
+
 -- --------------------------------------------------------
 
 --
@@ -59,6 +130,15 @@ CREATE TABLE `cthd_mua` (
   `thanhtien` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `cthd_mua`
+--
+
+INSERT INTO `cthd_mua` (`sohd_Mua`, `ma_SP`, `sl`, `thanhtien`) VALUES
+(1, 1, 10, 68000000),
+(2, 2, 20, 200000000),
+(3, 3, 10, 145000000);
+
 -- --------------------------------------------------------
 
 --
@@ -69,6 +149,18 @@ CREATE TABLE `ctkm` (
   `ma_KM` int(11) NOT NULL,
   `ma_SP` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ctkm`
+--
+
+INSERT INTO `ctkm` (`ma_KM`, `ma_SP`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(3, 1),
+(4, 2),
+(4, 3);
 
 -- --------------------------------------------------------
 
@@ -84,6 +176,17 @@ CREATE TABLE `hoadonban` (
   `tongtien_Ban` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `hoadonban`
+--
+
+INSERT INTO `hoadonban` (`sohd_Ban`, `ngay_Ban`, `ma_NV`, `ma_KH`, `tongtien_Ban`) VALUES
+(1, '2018-12-10', 4, 2, NULL),
+(2, '2018-12-10', 1, 4, NULL),
+(3, '2018-12-11', 1, 1, NULL),
+(4, '2018-12-13', 5, 3, NULL),
+(5, '2018-11-29', 1, 4, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -97,6 +200,16 @@ CREATE TABLE `hoadonmua` (
   `tongtien_Mua` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `hoadonmua`
+--
+
+INSERT INTO `hoadonmua` (`sohd_Mua`, `ngay_Nhap`, `ma_NCC`, `tongtien_Mua`) VALUES
+(1, '2018-12-04', 3, NULL),
+(2, '2018-12-10', 2, NULL),
+(3, '2018-12-11', 4, NULL),
+(4, '2018-12-15', 1, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -109,6 +222,15 @@ CREATE TABLE `hoahong` (
   `nam` int(4) NOT NULL,
   `tien_HH` double(255,0) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `hoahong`
+--
+
+INSERT INTO `hoahong` (`ma_NV`, `thang`, `nam`, `tien_HH`) VALUES
+(1, 8, 2018, NULL),
+(4, 11, 2018, NULL),
+(5, 11, 2018, NULL);
 
 -- --------------------------------------------------------
 
@@ -125,6 +247,17 @@ CREATE TABLE `khachhang` (
   `email` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `khachhang`
+--
+
+INSERT INTO `khachhang` (`ma_KH`, `ten_KH`, `soCMND_KH`, `diachi_KH`, `soDT_KH`, `email`) VALUES
+(1, 'Nguyễn Văn Hậu', 321643275, 'Bình Thạnh', 348249328, NULL),
+(2, 'Luis Alberto Suárez Díaz', 56789, 'Uruguay', 13456, NULL),
+(3, 'Edson Arantes do Nascimento', 6789, 'Brazil', 5432, NULL),
+(4, 'Vincent Willem van Gogh', 3444, 'Hà Lan', 333333, NULL),
+(5, 'William Sydney Porter', 32156, 'Mỹ', 777887, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -139,6 +272,17 @@ CREATE TABLE `khuyenmai` (
   `ngay_KT` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `khuyenmai`
+--
+
+INSERT INTO `khuyenmai` (`ma_KM`, `ten_KM`, `hs_KM`, `ngay_BD`, `ngay_KT`) VALUES
+(1, 'Không KM', 0, '2000-01-01', '2050-12-31'),
+(2, '30/4 - 1/5', 0.1, '2018-04-29', '2018-05-01'),
+(3, '20/11/2018', 0.05, '2018-11-19', '2018-11-20'),
+(4, 'Noel 2018', 0.07, '2018-12-23', '2018-12-25'),
+(5, 'Tết dương lịch 2019', 0.15, '2018-12-29', '2019-01-01');
+
 -- --------------------------------------------------------
 
 --
@@ -152,6 +296,16 @@ CREATE TABLE `nhacungcap` (
   `soDT_NCC` int(11) DEFAULT NULL,
   `tinh_trang` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `nhacungcap`
+--
+
+INSERT INTO `nhacungcap` (`ma_NCC`, `ten_NCC`, `diachi_NCC`, `soDT_NCC`, `tinh_trang`) VALUES
+(1, 'Alpha', 'Quận 2', 23456, 1),
+(2, 'Higa', 'Gò Vấp', 19567, 1),
+(3, 'LaLa', 'Quận 9', 666777, 1),
+(4, 'Syb', 'Bình Chánh', 5656, 1);
 
 -- --------------------------------------------------------
 
@@ -179,7 +333,9 @@ CREATE TABLE `nhanvien` (
 INSERT INTO `nhanvien` (`ma_NV`, `ten_NV`, `soCMND_NV`, `gioitinh`, `ngaysinh_NV`, `diachi_NV`, `soDT_NV`, `ngay_VaoLam`, `luong_CB`, `tinh_trang`) VALUES
 (1, 'thang', 147258369, 1, '2018-12-06', 'sdfsadfsd', 926528212, '2018-12-07', 123, 1),
 (2, 'Hồ Thái Thăng', 123456789, 1, '3898-02-01', 'ko có', 123456, '3918-11-01', 1000, 1),
-(3, 'Hồ Thái Thăng', 123456789, 1, '3898-02-01', 'ko có', 123456, '3918-11-01', 1000, 1);
+(3, 'Hồ Thái Thăng', 123456789, 1, '3898-02-01', 'ko có', 123456, '3918-11-01', 1000, 1),
+(4, 'Loui Pasteur', 345345, 1, '1822-12-27', 'Pháp', 123456, '2018-11-01', 2500000, 1),
+(5, 'Thomas Alva Edison', 7878, 1, '1847-02-11', 'Mỹ', 567432, '2018-01-01', 3000000, 1);
 
 -- --------------------------------------------------------
 
@@ -192,6 +348,15 @@ CREATE TABLE `nhasanxuat` (
   `ten_nsx` varchar(45) NOT NULL,
   `thongtin` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `nhasanxuat`
+--
+
+INSERT INTO `nhasanxuat` (`ma_nsx`, `ten_nsx`, `thongtin`) VALUES
+(1, 'ABC', ''),
+(2, 'XYZ', ''),
+(3, 'GHI', '');
 
 -- --------------------------------------------------------
 
@@ -235,6 +400,16 @@ CREATE TABLE `sanpham` (
   `tinhtrang` int(11) DEFAULT NULL,
   `heso_HH` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `sanpham`
+--
+
+INSERT INTO `sanpham` (`ma_SP`, `ten_SP`, `ma_nsx`, `sl`, `nam_SX`, `thue_VAT`, `gia_BanRa`, `thoigian_BH`, `xuatxu`, `mau`, `bonho`, `kichthuoc`, `anh`, `tinhtrang`, `heso_HH`) VALUES
+(1, 'iPhone 6 32GB', 1, 7, 2017, 0.1, 7000000, 12, 'Trung Quốc', 'Hồng', '32G', NULL, NULL, NULL, 0.06),
+(2, 'Samsung Galaxy A9', 3, 10, 2018, 0.2, 12500000, 12, NULL, 'Đen', '128GB', '6.3 inch Full HD+ (1080 x 2220 Pixels)', NULL, NULL, 0.05),
+(3, 'Samsung Galaxy S8', 2, 8, 2018, 0.2, 16000000, 12, NULL, 'Xanh', '64GB', '2K+ (1440 x 2960 Pixels)', NULL, 1, 0.06),
+(4, 'Oppo neo 9', 1, 5, 2017, 0.03, 5000000, 12, NULL, NULL, NULL, NULL, NULL, 1, 0.03);
 
 -- --------------------------------------------------------
 
@@ -336,7 +511,6 @@ ALTER TABLE `nhacungcap`
 --
 ALTER TABLE `nhanvien`
   ADD PRIMARY KEY (`ma_NV`);
--- ALTER TABLE `nhanvien` ADD FULLTEXT KEY `ten_NV` (`ten_NV`);
 
 --
 -- Indexes for table `nhasanxuat`
@@ -366,6 +540,76 @@ ALTER TABLE `taikhoan`
   ADD KEY `fk_TK_PQ` (`ma_PhanQuyen`);
 
 --
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `baohanh`
+--
+ALTER TABLE `baohanh`
+  MODIFY `ma_BH` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `ctkm`
+--
+ALTER TABLE `ctkm`
+  MODIFY `ma_KM` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `hoadonban`
+--
+ALTER TABLE `hoadonban`
+  MODIFY `sohd_Ban` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `hoadonmua`
+--
+ALTER TABLE `hoadonmua`
+  MODIFY `sohd_Mua` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `khachhang`
+--
+ALTER TABLE `khachhang`
+  MODIFY `ma_KH` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `khuyenmai`
+--
+ALTER TABLE `khuyenmai`
+  MODIFY `ma_KM` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `nhacungcap`
+--
+ALTER TABLE `nhacungcap`
+  MODIFY `ma_NCC` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  MODIFY `ma_NV` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `nhasanxuat`
+--
+ALTER TABLE `nhasanxuat`
+  MODIFY `ma_nsx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `phanquyen`
+--
+ALTER TABLE `phanquyen`
+  MODIFY `ma_PhanQuyen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `sanpham`
+--
+ALTER TABLE `sanpham`
+  MODIFY `ma_SP` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -373,43 +617,43 @@ ALTER TABLE `taikhoan`
 -- Constraints for table `baohanh`
 --
 ALTER TABLE `baohanh`
-  ADD CONSTRAINT `FK_t7bbmrq3qvuhygcy6wqq3robr` FOREIGN KEY (`ma_SP`) REFERENCES `sanpham` (`ma_SP`),
-  ADD CONSTRAINT `baohanh_ibfk_2` FOREIGN KEY (`sohd_Ban`) REFERENCES `hoadonban` (`sohd_Ban`);
+  ADD CONSTRAINT `baohanh_ibfk_1` FOREIGN KEY (`sohd_Ban`) REFERENCES `hoadonban` (`sohd_Ban`),
+  ADD CONSTRAINT `baohanh_ibfk_2` FOREIGN KEY (`ma_SP`) REFERENCES `sanpham` (`ma_SP`);
 
 --
 -- Constraints for table `cthd_ban`
 --
 ALTER TABLE `cthd_ban`
-  ADD CONSTRAINT `fk_CTHDB_HDB` FOREIGN KEY (`sohd_Ban`) REFERENCES `hoadonban` (`sohd_Ban`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_CTHDB_KM` FOREIGN KEY (`ma_KM`) REFERENCES `khuyenmai` (`ma_KM`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_CTHDB_SP` FOREIGN KEY (`ma_SP`) REFERENCES `sanpham` (`ma_SP`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `cthd_ban_ibfk_1` FOREIGN KEY (`sohd_Ban`) REFERENCES `hoadonban` (`sohd_Ban`),
+  ADD CONSTRAINT `cthd_ban_ibfk_2` FOREIGN KEY (`ma_SP`) REFERENCES `sanpham` (`ma_SP`),
+  ADD CONSTRAINT `cthd_ban_ibfk_3` FOREIGN KEY (`ma_KM`) REFERENCES `khuyenmai` (`ma_KM`);
 
 --
 -- Constraints for table `cthd_mua`
 --
 ALTER TABLE `cthd_mua`
-  ADD CONSTRAINT `fk_CTHDM_HDM` FOREIGN KEY (`sohd_Mua`) REFERENCES `hoadonmua` (`sohd_Mua`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_CTHDM_SP` FOREIGN KEY (`ma_SP`) REFERENCES `sanpham` (`ma_SP`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `cthd_mua_ibfk_1` FOREIGN KEY (`sohd_Mua`) REFERENCES `hoadonmua` (`sohd_Mua`),
+  ADD CONSTRAINT `cthd_mua_ibfk_2` FOREIGN KEY (`ma_SP`) REFERENCES `sanpham` (`ma_SP`);
 
 --
 -- Constraints for table `ctkm`
 --
 ALTER TABLE `ctkm`
-  ADD CONSTRAINT `fk_CTKM_KM` FOREIGN KEY (`ma_KM`) REFERENCES `khuyenmai` (`ma_KM`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_CTKM_SP` FOREIGN KEY (`ma_SP`) REFERENCES `sanpham` (`ma_SP`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `ctkm_ibfk_1` FOREIGN KEY (`ma_SP`) REFERENCES `sanpham` (`ma_SP`),
+  ADD CONSTRAINT `ctkm_ibfk_2` FOREIGN KEY (`ma_KM`) REFERENCES `khuyenmai` (`ma_KM`);
 
 --
 -- Constraints for table `hoadonban`
 --
 ALTER TABLE `hoadonban`
-  ADD CONSTRAINT `fk_hd_kh` FOREIGN KEY (`ma_KH`) REFERENCES `khachhang` (`ma_KH`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_hd_nv` FOREIGN KEY (`ma_NV`) REFERENCES `nhanvien` (`ma_NV`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `hoadonban_ibfk_1` FOREIGN KEY (`ma_NV`) REFERENCES `nhanvien` (`ma_NV`),
+  ADD CONSTRAINT `hoadonban_ibfk_2` FOREIGN KEY (`ma_KH`) REFERENCES `khachhang` (`ma_KH`);
 
 --
 -- Constraints for table `hoadonmua`
 --
 ALTER TABLE `hoadonmua`
-  ADD CONSTRAINT `fk_HDM_NCC` FOREIGN KEY (`ma_NCC`) REFERENCES `nhacungcap` (`ma_NCC`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `hoadonmua_ibfk_1` FOREIGN KEY (`ma_NCC`) REFERENCES `nhacungcap` (`ma_NCC`);
 
 --
 -- Constraints for table `hoahong`
@@ -418,22 +662,15 @@ ALTER TABLE `hoahong`
   ADD CONSTRAINT `hoahong_ibfk_1` FOREIGN KEY (`ma_NV`) REFERENCES `nhanvien` (`ma_NV`);
 
 --
--- Constraints for table `nhasanxuat`
---
-ALTER TABLE `nhasanxuat`
-  ADD CONSTRAINT `nhasanxuat_ibfk_1` FOREIGN KEY (`ma_nsx`) REFERENCES `sanpham` (`ma_nsx`);
-
---
 -- Constraints for table `sanpham`
 --
 ALTER TABLE `sanpham`
-  ADD CONSTRAINT `FK_4wgoljn2csa1vdiy58t7twma6` FOREIGN KEY (`ma_nsx`) REFERENCES `nhasanxuat` (`ma_nsx`),
-  ADD CONSTRAINT `sanpham_ibfk_1` FOREIGN KEY (`ma_SP`) REFERENCES `baohanh` (`ma_SP`);
+  ADD CONSTRAINT `sanpham_ibfk_1` FOREIGN KEY (`ma_nsx`) REFERENCES `nhasanxuat` (`ma_nsx`);
 
 --
 -- Constraints for table `taikhoan`
 --
 ALTER TABLE `taikhoan`
-  ADD CONSTRAINT `fk_TK_NV` FOREIGN KEY (`ma_NV`) REFERENCES `nhanvien` (`ma_NV`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_TK_PQ` FOREIGN KEY (`ma_PhanQuyen`) REFERENCES `phanquyen` (`ma_PhanQuyen`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `taikhoan_ibfk_1` FOREIGN KEY (`ma_NV`) REFERENCES `nhanvien` (`ma_NV`),
+  ADD CONSTRAINT `taikhoan_ibfk_2` FOREIGN KEY (`ma_PhanQuyen`) REFERENCES `phanquyen` (`ma_PhanQuyen`);
 COMMIT;
