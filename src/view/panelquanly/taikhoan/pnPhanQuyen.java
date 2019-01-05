8b81a2d2e9c8a3477046bb60ac3e5d4ca9c98e0e
doublefeel98 <thanglong2098@gmail.com>
2019-01-05 14:51:08 +0700

    màn hình tài khoản và phân quyền

--- src/view/panelquanly/taikhoan/pnPhanQuyen.java
+++ src/view/panelquanly/taikhoan/pnPhanQuyen.java
@@ -5,25 +5,36 @@
  */
 package view.panelquanly.taikhoan;
 
+import controller.PhanQuyenController;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.TableModel;
+import model.PhanQuyenModelTable;
+import model.entities.PhanQuyen;
+import view.interfaceView.iMessageView;
+import view.interfaceView.iModelTable;
 
 /**
  *
  * @author RanRan
  */
-public class pnPhanQuyen extends JPanel {
+public class pnPhanQuyen extends JPanel implements iMessageView, iModelTable {
 
     private JTextField tfMaPhanQuyen;
     private JTextField tfQuyenTruyCap;
@@ -32,90 +43,183 @@ public class pnPhanQuyen extends JPanel {
     private JButton btnHuyPQ;
     private JTable tablePhanQuyen;
 
-    public pnPhanQuyen(){
+    private PhanQuyen phanQuyen;
+
+    public pnPhanQuyen() {
         initComponent();
+        initData();
+        initEvent();
     }
 
     private void initComponent() {
 
-                setBorder(new LineBorder(new Color(0, 51, 51), 2));
-		setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel_53 = new JPanel();
-		add(panel_53, BorderLayout.NORTH);
-		
-		JLabel lblTieuDeQLPQ = new JLabel("QUẢN LÝ PHÂN QUYỀN");
-		lblTieuDeQLPQ.setForeground(new Color(0, 51, 51));
-		lblTieuDeQLPQ.setFont(new Font("Tahoma", Font.BOLD, 25));
-		panel_53.add(lblTieuDeQLPQ);
-		
-		JPanel panel_54 = new JPanel();
-		add(panel_54, BorderLayout.CENTER);
-		panel_54.setLayout(new GridLayout(2, 0, 0, 0));
-		
-		JPanel panel_55 = new JPanel();
-		panel_54.add(panel_55);
-		panel_55.setLayout(new GridLayout(6, 0, 0, 0));
-		
-		JPanel pnMaPhanQuyen = new JPanel();
-		FlowLayout flowLayout_83 = (FlowLayout) pnMaPhanQuyen.getLayout();
-		flowLayout_83.setVgap(10);
-		flowLayout_83.setHgap(10);
-		flowLayout_83.setAlignment(FlowLayout.LEFT);
-		panel_55.add(pnMaPhanQuyen);
-		
-		JLabel lblMaPhanQuyen = new JLabel("Mã phân quyền:");
-		lblMaPhanQuyen.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		pnMaPhanQuyen.add(lblMaPhanQuyen);
-		
-		tfMaPhanQuyen = new JTextField();
-		tfMaPhanQuyen.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		tfMaPhanQuyen.setColumns(20);
-		pnMaPhanQuyen.add(tfMaPhanQuyen);
-		
-		JPanel pnQuyenTruyCap = new JPanel();
-		FlowLayout flowLayout_84 = (FlowLayout) pnQuyenTruyCap.getLayout();
-		flowLayout_84.setVgap(10);
-		flowLayout_84.setHgap(10);
-		flowLayout_84.setAlignment(FlowLayout.LEFT);
-		panel_55.add(pnQuyenTruyCap);
-		
-		JLabel lblQuyenTruyCap = new JLabel("Quyền truy cập:");
-		lblQuyenTruyCap.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		pnQuyenTruyCap.add(lblQuyenTruyCap);
-		
-		tfQuyenTruyCap = new JTextField();
-		tfQuyenTruyCap.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		tfQuyenTruyCap.setColumns(20);
-		pnQuyenTruyCap.add(tfQuyenTruyCap);
-		
-		JPanel pnButtonQLPQ = new JPanel();
-		FlowLayout flowLayout_85 = (FlowLayout) pnButtonQLPQ.getLayout();
-		flowLayout_85.setVgap(10);
-		flowLayout_85.setHgap(10);
-		flowLayout_85.setAlignment(FlowLayout.RIGHT);
-		panel_55.add(pnButtonQLPQ);
-		
-		btnTimKiemPQ = new JButton("Tìm kiếm");
-		btnTimKiemPQ.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		pnButtonQLPQ.add(btnTimKiemPQ);
-		
-		btnCapNhatPQ = new JButton("Cập nhật");
-		btnCapNhatPQ.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		pnButtonQLPQ.add(btnCapNhatPQ);
-		
-		btnHuyPQ = new JButton("Hủy");
-		btnHuyPQ.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		pnButtonQLPQ.add(btnHuyPQ);
-		
-		JPanel panel_61 = new JPanel();
-		panel_54.add(panel_61);
-		panel_61.setLayout(new BorderLayout(0, 0));
-		
-		JScrollPane scrollPaneTablePhanQuyen = new JScrollPane();
-		panel_61.add(scrollPaneTablePhanQuyen, BorderLayout.CENTER);
-		
-		tablePhanQuyen = new JTable();
-		scrollPaneTablePhanQuyen.setViewportView(tablePhanQuyen);		
+        setBorder(new LineBorder(new Color(0, 51, 51), 2));
+        setLayout(new BorderLayout(0, 0));
+
+        JPanel panel_53 = new JPanel();
+        add(panel_53, BorderLayout.NORTH);
+
+        JLabel lblTieuDeQLPQ = new JLabel("QUẢN LÝ PHÂN QUYỀN");
+        lblTieuDeQLPQ.setForeground(new Color(0, 51, 51));
+        lblTieuDeQLPQ.setFont(new Font("Tahoma", Font.BOLD, 25));
+        panel_53.add(lblTieuDeQLPQ);
+
+        JPanel panel_54 = new JPanel();
+        add(panel_54, BorderLayout.CENTER);
+        panel_54.setLayout(new GridLayout(2, 0, 0, 0));
+
+        JPanel panel_55 = new JPanel();
+        panel_54.add(panel_55);
+        panel_55.setLayout(new GridLayout(6, 0, 0, 0));
+
+        JPanel pnMaPhanQuyen = new JPanel();
+        FlowLayout flowLayout_83 = (FlowLayout) pnMaPhanQuyen.getLayout();
+        flowLayout_83.setVgap(10);
+        flowLayout_83.setHgap(10);
+        flowLayout_83.setAlignment(FlowLayout.LEFT);
+        panel_55.add(pnMaPhanQuyen);
+
+        JLabel lblMaPhanQuyen = new JLabel("Mã phân quyền:");
+        lblMaPhanQuyen.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        pnMaPhanQuyen.add(lblMaPhanQuyen);
+
+        tfMaPhanQuyen = new JTextField();
+        tfMaPhanQuyen.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        tfMaPhanQuyen.setColumns(20);
+        pnMaPhanQuyen.add(tfMaPhanQuyen);
+
+        JPanel pnQuyenTruyCap = new JPanel();
+        FlowLayout flowLayout_84 = (FlowLayout) pnQuyenTruyCap.getLayout();
+        flowLayout_84.setVgap(10);
+        flowLayout_84.setHgap(10);
+        flowLayout_84.setAlignment(FlowLayout.LEFT);
+        panel_55.add(pnQuyenTruyCap);
+
+        JLabel lblQuyenTruyCap = new JLabel("Quyền truy cập:");
+        lblQuyenTruyCap.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        pnQuyenTruyCap.add(lblQuyenTruyCap);
+
+        tfQuyenTruyCap = new JTextField();
+        tfQuyenTruyCap.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        tfQuyenTruyCap.setColumns(20);
+        pnQuyenTruyCap.add(tfQuyenTruyCap);
+
+        JPanel pnButtonQLPQ = new JPanel();
+        FlowLayout flowLayout_85 = (FlowLayout) pnButtonQLPQ.getLayout();
+        flowLayout_85.setVgap(10);
+        flowLayout_85.setHgap(10);
+        flowLayout_85.setAlignment(FlowLayout.RIGHT);
+        panel_55.add(pnButtonQLPQ);
+
+        btnTimKiemPQ = new JButton("Tìm kiếm");
+        btnTimKiemPQ.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        //pnButtonQLPQ.add(btnTimKiemPQ);
+
+        btnCapNhatPQ = new JButton("Cập nhật");
+        btnCapNhatPQ.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        pnButtonQLPQ.add(btnCapNhatPQ);
+
+        btnHuyPQ = new JButton("Hủy");
+        btnHuyPQ.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        pnButtonQLPQ.add(btnHuyPQ);
+
+        JPanel panel_61 = new JPanel();
+        panel_54.add(panel_61);
+        panel_61.setLayout(new BorderLayout(0, 0));
+
+        JScrollPane scrollPaneTablePhanQuyen = new JScrollPane();
+        panel_61.add(scrollPaneTablePhanQuyen, BorderLayout.CENTER);
+
+        tablePhanQuyen = new JTable();
+        scrollPaneTablePhanQuyen.setViewportView(tablePhanQuyen);
+    }
+
+    @Override
+    public void showMessageAndReloadData(String message, int type) {
+        JOptionPane.showMessageDialog(null, message, "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+        switch (type) {
+            case iMessageView.NONE:
+                break;
+            case iMessageView.FAIL:
+                break;
+            case iMessageView.SUCCESS:
+                loadToanBoPhanQuyenLenTable();
+                clearData();
+                break;
+        }
+    }
+
+    @Override
+    public void hienThiDuLieuLenTable(TableModel tableModel) {
+        if (tableModel == null) {
+            return;
+        }
+        tablePhanQuyen.setModel(tableModel);
+    }
+
+    private void clearData() {
+
+    }
+
+    private void initData() {
+        loadToanBoPhanQuyenLenTable();
+    }
+
+    private void initEvent() {
+        tablePhanQuyen.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+                    tablePhanQuyenSelection();
+                }
+            }
+        });
+
+        btnHuyPQ.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearData();
+            }
+        });
+        
+        btnCapNhatPQ.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                capNhatPhanQUyen();
+            }
+        });
+    }
+
+    private void loadToanBoPhanQuyenLenTable() {
+        PhanQuyenController.getInstance().layToanBoDuLieuLenTable(this);
+    }
+
+    private void tablePhanQuyenSelection() {
+        int row = tablePhanQuyen.getSelectedRow();
+        if (row < 0) {
+            return;
+        }
+
+        PhanQuyenModelTable modelTable = (PhanQuyenModelTable) tablePhanQuyen.getModel();
+        phanQuyen = modelTable.getSelectedRow(row);
+
+        String maPQ = phanQuyen.getMaPhanQuyen() + "";
+        String quyenTruyCap = phanQuyen.getQuyentruycap();
+
+        hienThiThongTinPhanQuyen(maPQ, quyenTruyCap);
+    }
+
+    private void hienThiThongTinPhanQuyen(String maPQ, String quyenTruyCap) {
+        tfMaPhanQuyen.setText(maPQ);
+        tfQuyenTruyCap.setText(quyenTruyCap);
+    }
+
+    private void capNhatPhanQUyen() {
+        if(phanQuyen == null) return;
+        
+        String quyenTruyCap = tfQuyenTruyCap.getText();
+        
+        phanQuyen.setQuyentruycap(quyenTruyCap);
+        PhanQuyenController.getInstance().capNhatThongTinNhaCungCap(phanQuyen, this);
     }
 }
--- src/view/panelquanly/taikhoan/pnTaiKhoan.java
+++ src/view/panelquanly/taikhoan/pnTaiKhoan.java
@@ -5,28 +5,44 @@
  */
 package view.panelquanly.taikhoan;
 
+import controller.PhanQuyenController;
+import controller.TaiKhoanController;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.TableModel;
+import model.TaiKhoanModelTable;
+import model.entities.PhanQuyen;
+import model.entities.TaiKhoan;
+import view.interfaceView.iMessageView;
+import view.interfaceView.iModelComBox;
+import view.interfaceView.iModelTable;
 
 /**
  *
  * @author RanRan
  */
-public class pnTaiKhoan extends JPanel{
-    
-    private JTextField tfMaTaiKhoan;
+public class pnTaiKhoan extends JPanel implements iMessageView, iModelComBox, iModelTable {
+
+//    private JTextField tfMaTaiKhoan;
     private JTextField tfTenDangNhap;
     private JTextField tfNhanVienTK;
     private JComboBox cbbPhanQuyenTK;
@@ -34,126 +50,269 @@ public class pnTaiKhoan extends JPanel{
     private JButton btnResetMatKhau;
     private JButton btnHuyTK;
     private JTable tableTaiKhoan;
-    
-    public pnTaiKhoan(){
+
+    private TaiKhoan taiKhoan;
+    private JButton btnCapNhatTK;
+
+    public pnTaiKhoan() {
         initComponent();
+        initData();
+        initEvent();
     }
 
     private void initComponent() {
-                setBorder(new LineBorder(new Color(0, 51, 51), 2));
-		setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel_44 = new JPanel();
-		add(panel_44, BorderLayout.NORTH);
-		
-		JLabel lblTieuDeQLTK = new JLabel("QUẢN LÝ TÀI KHOẢN");
-		lblTieuDeQLTK.setForeground(new Color(0, 51, 51));
-		lblTieuDeQLTK.setFont(new Font("Tahoma", Font.BOLD, 25));
-		panel_44.add(lblTieuDeQLTK);
-		
-		JPanel panel_45 = new JPanel();
-		add(panel_45);
-		panel_45.setLayout(new GridLayout(2, 0, 0, 0));
-		
-		JPanel panel_46 = new JPanel();
-		panel_45.add(panel_46);
-		panel_46.setLayout(new GridLayout(6, 0, 0, 0));
-		
-		JPanel pnMaTK = new JPanel();
-		FlowLayout flowLayout_81 = (FlowLayout) pnMaTK.getLayout();
-		flowLayout_81.setAlignment(FlowLayout.LEFT);
-		flowLayout_81.setVgap(10);
-		flowLayout_81.setHgap(10);
-		panel_46.add(pnMaTK);
-		
-		JLabel lblMaTaiKhoan = new JLabel("Mã tài khoản:   ");
-		lblMaTaiKhoan.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		pnMaTK.add(lblMaTaiKhoan);
-		
-		tfMaTaiKhoan = new JTextField();
-		tfMaTaiKhoan.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		tfMaTaiKhoan.setColumns(20);
-		pnMaTK.add(tfMaTaiKhoan);
-		
-		JPanel pnTenDangNhap = new JPanel();
-		FlowLayout flowLayout_82 = (FlowLayout) pnTenDangNhap.getLayout();
-		flowLayout_82.setVgap(10);
-		flowLayout_82.setHgap(10);
-		flowLayout_82.setAlignment(FlowLayout.LEFT);
-		panel_46.add(pnTenDangNhap);
-		
-		JLabel lblTenDangNhap = new JLabel("Tên đăng nhập:");
-		lblTenDangNhap.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		pnTenDangNhap.add(lblTenDangNhap);
-		
-		tfTenDangNhap = new JTextField();
-		tfTenDangNhap.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		tfTenDangNhap.setColumns(20);
-		pnTenDangNhap.add(tfTenDangNhap);
-		
-		JPanel pnNhanVienTK = new JPanel();
-		FlowLayout fl_pnNhanVienTK = (FlowLayout) pnNhanVienTK.getLayout();
-		fl_pnNhanVienTK.setVgap(10);
-		fl_pnNhanVienTK.setHgap(10);
-		fl_pnNhanVienTK.setAlignment(FlowLayout.LEFT);
-		panel_46.add(pnNhanVienTK);
-		
-		JLabel lblNhanVienTK = new JLabel("Nhân viên:       ");
-		lblNhanVienTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		pnNhanVienTK.add(lblNhanVienTK);
-		
-		tfNhanVienTK = new JTextField();
-		tfNhanVienTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		tfNhanVienTK.setColumns(20);
-		pnNhanVienTK.add(tfNhanVienTK);
-		
-		JPanel pnPhanQuyenTK = new JPanel();
-		FlowLayout fl_pnPhanQuyenTK = (FlowLayout) pnPhanQuyenTK.getLayout();
-		fl_pnPhanQuyenTK.setVgap(10);
-		fl_pnPhanQuyenTK.setHgap(10);
-		fl_pnPhanQuyenTK.setAlignment(FlowLayout.LEFT);
-		panel_46.add(pnPhanQuyenTK);
-		
-		JLabel lblPhanQuyenTK = new JLabel("Phân quyền:     ");
-		lblPhanQuyenTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		pnPhanQuyenTK.add(lblPhanQuyenTK);
-		
-		cbbPhanQuyenTK = new JComboBox();
-		cbbPhanQuyenTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
-                cbbPhanQuyenTK.setPreferredSize(new Dimension(150, 25));
-                cbbPhanQuyenTK.setMaximumSize(new Dimension(150, 25));
-                cbbPhanQuyenTK.setMinimumSize(new Dimension(150, 25));
-		pnPhanQuyenTK.add(cbbPhanQuyenTK);
-		
-		JPanel pnButtonQLTK = new JPanel();
-		FlowLayout fl_pnButtonQLTK = (FlowLayout) pnButtonQLTK.getLayout();
-		fl_pnButtonQLTK.setAlignment(FlowLayout.RIGHT);
-		fl_pnButtonQLTK.setVgap(10);
-		fl_pnButtonQLTK.setHgap(10);
-		panel_46.add(pnButtonQLTK);
-		
-		btnTimKiemTK = new JButton("Tìm kiếm");
-		btnTimKiemTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		pnButtonQLTK.add(btnTimKiemTK);
-		
-		btnResetMatKhau = new JButton("Đặt lại mật khẩu");
-		btnResetMatKhau.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		pnButtonQLTK.add(btnResetMatKhau);
-		
-		btnHuyTK = new JButton("Hủy");
-		btnHuyTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		pnButtonQLTK.add(btnHuyTK);
-		
-		JPanel panel_52 = new JPanel();
-		panel_45.add(panel_52);
-		panel_52.setLayout(new BorderLayout(0, 0));
-		
-		JScrollPane scrollPaneTableTaiKhoan = new JScrollPane();
-		panel_52.add(scrollPaneTableTaiKhoan, BorderLayout.CENTER);
-		
-		tableTaiKhoan = new JTable();
-		scrollPaneTableTaiKhoan.setViewportView(tableTaiKhoan);
+        setBorder(new LineBorder(new Color(0, 51, 51), 2));
+        setLayout(new BorderLayout(0, 0));
+
+        JPanel panel_44 = new JPanel();
+        add(panel_44, BorderLayout.NORTH);
+
+        JLabel lblTieuDeQLTK = new JLabel("QUẢN LÝ TÀI KHOẢN");
+        lblTieuDeQLTK.setForeground(new Color(0, 51, 51));
+        lblTieuDeQLTK.setFont(new Font("Tahoma", Font.BOLD, 25));
+        panel_44.add(lblTieuDeQLTK);
+
+        JPanel panel_45 = new JPanel();
+        add(panel_45);
+        panel_45.setLayout(new GridLayout(2, 0, 0, 0));
+
+        JPanel panel_46 = new JPanel();
+        panel_45.add(panel_46);
+        panel_46.setLayout(new GridLayout(6, 0, 0, 0));
+
+//        JPanel pnMaTK = new JPanel();
+//        FlowLayout flowLayout_81 = (FlowLayout) pnMaTK.getLayout();
+//        flowLayout_81.setAlignment(FlowLayout.LEFT);
+//        flowLayout_81.setVgap(10);
+//        flowLayout_81.setHgap(10);
+//        panel_46.add(pnMaTK);
+//        JLabel lblMaTaiKhoan = new JLabel("Mã tài khoản:   ");
+//        lblMaTaiKhoan.setFont(new Font("Tahoma", Font.PLAIN, 15));
+//        pnMaTK.add(lblMaTaiKhoan);
+//
+//        tfMaTaiKhoan = new JTextField();
+//        tfMaTaiKhoan.setFont(new Font("Tahoma", Font.PLAIN, 15));
+//        tfMaTaiKhoan.setColumns(20);
+//        pnMaTK.add(tfMaTaiKhoan);
+        JPanel pnTenDangNhap = new JPanel();
+        FlowLayout flowLayout_82 = (FlowLayout) pnTenDangNhap.getLayout();
+        flowLayout_82.setVgap(10);
+        flowLayout_82.setHgap(10);
+        flowLayout_82.setAlignment(FlowLayout.LEFT);
+        panel_46.add(pnTenDangNhap);
+
+        JLabel lblTenDangNhap = new JLabel("Tên đăng nhập:");
+        lblTenDangNhap.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        pnTenDangNhap.add(lblTenDangNhap);
+
+        tfTenDangNhap = new JTextField();
+        tfTenDangNhap.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        tfTenDangNhap.setColumns(20);
+        pnTenDangNhap.add(tfTenDangNhap);
+
+        JPanel pnNhanVienTK = new JPanel();
+        FlowLayout fl_pnNhanVienTK = (FlowLayout) pnNhanVienTK.getLayout();
+        fl_pnNhanVienTK.setVgap(10);
+        fl_pnNhanVienTK.setHgap(10);
+        fl_pnNhanVienTK.setAlignment(FlowLayout.LEFT);
+        panel_46.add(pnNhanVienTK);
+
+        JLabel lblNhanVienTK = new JLabel("Nhân viên:       ");
+        lblNhanVienTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        pnNhanVienTK.add(lblNhanVienTK);
+
+        tfNhanVienTK = new JTextField();
+        tfNhanVienTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        tfNhanVienTK.setColumns(20);
+        pnNhanVienTK.add(tfNhanVienTK);
+
+        JPanel pnPhanQuyenTK = new JPanel();
+        FlowLayout fl_pnPhanQuyenTK = (FlowLayout) pnPhanQuyenTK.getLayout();
+        fl_pnPhanQuyenTK.setVgap(10);
+        fl_pnPhanQuyenTK.setHgap(10);
+        fl_pnPhanQuyenTK.setAlignment(FlowLayout.LEFT);
+        panel_46.add(pnPhanQuyenTK);
+
+        JLabel lblPhanQuyenTK = new JLabel("Phân quyền:     ");
+        lblPhanQuyenTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        pnPhanQuyenTK.add(lblPhanQuyenTK);
+
+        cbbPhanQuyenTK = new JComboBox();
+        cbbPhanQuyenTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        cbbPhanQuyenTK.setPreferredSize(new Dimension(150, 25));
+        cbbPhanQuyenTK.setMaximumSize(new Dimension(150, 25));
+        cbbPhanQuyenTK.setMinimumSize(new Dimension(150, 25));
+        pnPhanQuyenTK.add(cbbPhanQuyenTK);
+
+        JPanel pnButtonQLTK = new JPanel();
+        FlowLayout fl_pnButtonQLTK = (FlowLayout) pnButtonQLTK.getLayout();
+        fl_pnButtonQLTK.setAlignment(FlowLayout.RIGHT);
+        fl_pnButtonQLTK.setVgap(10);
+        fl_pnButtonQLTK.setHgap(10);
+        panel_46.add(pnButtonQLTK);
+
+        btnTimKiemTK = new JButton("Tìm kiếm");
+        btnTimKiemTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        pnButtonQLTK.add(btnTimKiemTK);
+
+        btnCapNhatTK = new JButton("Cập Nhật");
+        btnCapNhatTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        pnButtonQLTK.add(btnCapNhatTK);
+
+        btnResetMatKhau = new JButton("Đặt lại mật khẩu");
+        btnResetMatKhau.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        pnButtonQLTK.add(btnResetMatKhau);
+
+        btnHuyTK = new JButton("Hủy");
+        btnHuyTK.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        pnButtonQLTK.add(btnHuyTK);
+
+        JPanel panel_52 = new JPanel();
+        panel_45.add(panel_52);
+        panel_52.setLayout(new BorderLayout(0, 0));
+
+        JScrollPane scrollPaneTableTaiKhoan = new JScrollPane();
+        panel_52.add(scrollPaneTableTaiKhoan, BorderLayout.CENTER);
+
+        tableTaiKhoan = new JTable();
+        scrollPaneTableTaiKhoan.setViewportView(tableTaiKhoan);
     }
-  
-    
+
+    private void initData() {
+        loadToanBoTaiKhoanLenTable();
+        loadDataCbbPhanQuyen();
+    }
+
+    private void initEvent() {
+        tableTaiKhoan.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+                    tableTaiKhoanSelection();
+                }
+            }
+        });
+
+        btnHuyTK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearData();
+            }
+        });
+
+        btnResetMatKhau.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resetMatKhau();
+            }
+        });
+
+        btnTimKiemTK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                timKiemTaiKhoanTheoTenDangNhap();
+            }
+        });
+
+        btnCapNhatTK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                capNhatTaiKhoan();
+            }
+        });
+    }
+
+    @Override
+    public void showMessageAndReloadData(String message, int type) {
+        JOptionPane.showMessageDialog(null, message, "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+        switch (type) {
+            case iMessageView.NONE:
+                break;
+            case iMessageView.FAIL:
+                break;
+            case iMessageView.SUCCESS:
+                loadToanBoTaiKhoanLenTable();
+                clearData();
+                break;
+        }
+    }
+
+    @Override
+    public void hienThiDuLieuLenComBox(List data, Object object) {
+        cbbPhanQuyenTK.removeAllItems();
+        for (Iterator it = data.iterator(); it.hasNext();) {
+            PhanQuyen pq = (PhanQuyen) it.next();
+            cbbPhanQuyenTK.addItem(pq);
+        }
+        cbbPhanQuyenTK.setSelectedIndex(-1);
+    }
+
+    @Override
+    public void hienThiDuLieuLenTable(TableModel tableModel) {
+        if (tableModel == null) {
+            return;
+        }
+        tableTaiKhoan.setModel(tableModel);
+    }
+
+    private void loadToanBoTaiKhoanLenTable() {
+        TaiKhoanController.getInstance().layToanBoDuLieuLenTable(this);
+    }
+
+    private void loadDataCbbPhanQuyen() {
+        PhanQuyenController.getInstance().layToanBoDuLieuLenComBox(this);
+    }
+
+    private void clearData() {
+
+    }
+
+    private void tableTaiKhoanSelection() {
+        int row = tableTaiKhoan.getSelectedRow();
+        if (row < 0) {
+            return;
+        }
+
+        TaiKhoanModelTable modelTable = (TaiKhoanModelTable) tableTaiKhoan.getModel();
+        taiKhoan = modelTable.getSelectedRow(row);
+
+        String tenDangNhap = taiKhoan.getTenDangNhap();
+        String tenNV = "";
+        if (taiKhoan.getNhanvien() != null) {
+            tenNV = taiKhoan.getNhanvien().getTenNv();
+        }
+        PhanQuyen phanQuyen = taiKhoan.getPhanquyen();
+        hienThiThongTinTaiKhoan(tenDangNhap, tenNV, phanQuyen);
+    }
+
+    private void resetMatKhau() {
+        if (taiKhoan == null) {
+            return;
+        }
+        TaiKhoanController.getInstance().resetMatKhau(taiKhoan, this);
+    }
+
+    private void hienThiThongTinTaiKhoan(String tenDangNhap, String tenNV, PhanQuyen phanQuyen) {
+        tfTenDangNhap.setText(tenDangNhap);
+        tfNhanVienTK.setText(tenNV);
+
+        if (phanQuyen != null) {
+            cbbPhanQuyenTK.getModel().setSelectedItem(phanQuyen);
+        }
+    }
+
+    private void timKiemTaiKhoanTheoTenDangNhap() {
+        String tenDangNhap = tfTenDangNhap.getText();
+        TaiKhoanController.getInstance().timKiemTaiKhoanTheoTenDangNhapLenTable(tenDangNhap, this);
+    }
+
+    private void capNhatTaiKhoan() {
+        if(taiKhoan == null) return;
+        PhanQuyen phanQuyen = (PhanQuyen) cbbPhanQuyenTK.getSelectedItem();
+        if(phanQuyen == null) return;
+        
+        taiKhoan.setPhanquyen(phanQuyen);
+        
+        TaiKhoanController.getInstance();
+    }
 }
